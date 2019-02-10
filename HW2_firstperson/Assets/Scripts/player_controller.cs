6ff3dd66811c87e2bcc87d791be8f003792f9513
monte0 <47285725+monte0@users.noreply.github.com>
2019-02-09 19:22:05 -0800

    more

---
+++ HW2_firstperson/Assets/Scripts/player_controller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class player_controller : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move_horizontal = Input.GetAxis("Horizontal");
+        float move_vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(move_horizontal, 0f, move_vertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
