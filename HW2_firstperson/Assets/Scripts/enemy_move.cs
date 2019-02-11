81552b2f9911095c109a841e5353ded3322bfc49
monte0 <47285725+monte0@users.noreply.github.com>
2019-02-11 13:09:15 -0800

    health

---
+++ HW2_firstperson/Assets/Scripts/enemy_move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_move : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player, Vector3.up);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
