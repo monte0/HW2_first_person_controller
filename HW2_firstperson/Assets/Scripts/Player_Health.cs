0c3ab39065f9163a9c973b0ca374413669ace82b
monte0 <47285725+monte0@users.noreply.github.com>
2019-02-11 14:50:09 -0800

    enemy attack

---
+++ HW2_firstperson/Assets/Scripts/Player_Health.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour
+{
+    public int startingHealth;
+    public int currentHealth;
+    public Slider HealthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashcolor = new Color(1f, 0f, 0f, 0.1f);
+
+    contr playerMovement;
+    bool isDead;
+    bool damaged;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement.GetComponent<contr>();
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(damaged)
+        {
+            damageImage.color = flashcolor;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        HealthSlider.value = currentHealth;
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+    void Death()
+    {
+        isDead = true;
+        playerMovement.enabled = false;
+    }
+}
---
+++ HW2_firstperson/Assets/Scripts/enemy_attack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_attack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage;
+
+    GameObject player;
+    Player_Health playerHealth;
+    //Enemy_Health enemyHealth;
+    bool playerInRange;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<Player_Health>();
+        //enemyHealth GetComponent<Enemy_Health>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenAttacks && playerInRange/*&& enemyHealth.currentHealth > 0 */)
+        {
+            Attack();
+        }
+        if(playerHealth.currentHealth <= 0)
+        {
+            //isDead anim
+        }
+    }
+    void Attack()
+    {
+        timer = 0f;
+        if(playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
