e84e04d47c3369b2b6e00f62ccdf2d17b6b50f4e
monte0 <47285725+monte0@users.noreply.github.com>
2019-02-10 20:32:31 -0800

    more

---
+++ HW2_firstperson/Assets/Scripts/contr.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class contr : MonoBehaviour
+{
+    public float speed;
+    public float gravity;
+    public float jumpHeight;
+    public LayerMask ground;
+    public Transform player;
+    private Rigidbody rb;
+
+    private Vector3 direction;
+    private Vector3 walkingVelocity;
+    private Vector3 fallingVelocity;
+    private CharacterController controller;
+
+    // Use this for initialization
+
+    void Start()
+    {
+        gravity = 9.8f;
+        jumpHeight = 3.0f;
+        direction = Vector3.zero;
+        fallingVelocity = Vector3.zero;
+        controller = GetComponent<CharacterController>();
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        direction = direction.normalized;
+        walkingVelocity = direction * speed;
+        controller.Move(walkingVelocity * Time.deltaTime);
+
+        if (direction != Vector3.zero)
+        {
+            transform.forward = direction;
+            Debug.Log(direction);
+        }
+
+        bool isGrounded = Physics.CheckSphere(player.position, 0.1f, ground, QueryTriggerInteraction.Ignore);
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+        if (controller.isGrounded)
+            fallingVelocity.y = 0f;
+        else
+            fallingVelocity.y -= gravity * Time.deltaTime;
+   
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            fallingVelocity.y = Mathf.Sqrt(gravity * jumpHeight);
+        }
+        controller.Move(fallingVelocity * Time.deltaTime);
+    }
+}
---
+++ HW2_firstperson/Assets/Scripts/first.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+  //  public LayerMask ground;
+  //  public Transform feet;
+
+
+
+    private Vector3 direction;
+    private Rigidbody rbody;
+
+    private float rotationSpeed = 1f;
+    private float minY = -60f;
+    private float maxY = 60f;
+    private float rotationY = 0f;
+    private float rotationX = 0f;
+
+    // Use this for initialization
+
+    void Start()
+    {
+        speed = 5.0f;
+        jumpHeight = 3.0f;
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        direction = direction.normalized;
+        if (direction.x != 0)
+            rbody.MovePosition(rbody.position + transform.right * direction.x * speed * Time.deltaTime);
+        if (direction.z != 0)
+            rbody.MovePosition(rbody.position + transform.forward * direction.z * speed * Time.deltaTime);
+
+        rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotationSpeed;
+        rotationY += Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+}
